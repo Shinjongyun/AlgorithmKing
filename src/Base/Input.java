1f2abb698e11ac6653de2d3bab4a1806fc4ff9ec
Shinjongyun <tigerjong123@gmail.com>
2025-07-01 09:50:28 +0900

    Initial Commit

---
+++ src/Base/Input.java
@@ -0,0 +1,21 @@
+package Base;
+
+import java.io.*;
+import java.util.*;
+
+public class Input {
+    public static void main(String[] args) throws IOException {
+        // 빠른 입력을 위한 BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 한 줄 입력 받아서 공백 기준으로 나누기
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 예: 정수 두 개 입력 받기
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // 예시 출력
+        System.out.println("N = " + N + ", M = " + M);
+    }
+}
