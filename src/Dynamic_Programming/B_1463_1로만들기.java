eb249f86d4be8c62628bb5a4d4dfddc63fcdac92
Shinjongyun <tigerjong123@gmail.com>
2026-01-26 17:18:52 +0900

    1463 1로 만들기

---
+++ src/Dynamic_Programming/B_1463_1로만들기.java
@@ -0,0 +1,28 @@
+package Dynamic_Programming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_1463_1로만들기 {
+    static int X;
+    static int[] dp;
+    static int answer;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        X = Integer.parseInt(br.readLine());
+        dp = new int[X+1];
+        answer = 0;
+
+        dp[1] = 0;
+
+        for (int i = 2; i <= X; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
+
+        answer = dp[X];
+        System.out.print(answer);
+    }
+}
