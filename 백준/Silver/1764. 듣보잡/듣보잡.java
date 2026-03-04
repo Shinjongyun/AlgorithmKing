a605e5e5e5b0ad7de9c78a3e72a69fb2ac6ee859
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-03-04 17:19:23 +0900

    [Silver IV] Title: 듣보잡, Time: 252 ms, Memory: 26352 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,44 @@
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static Set<String> set = new HashSet<>();
+    static int N;
+    static int M;
+    static StringBuilder sb = new StringBuilder();
+    static int count;
+    static List<String> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            String name = br.readLine();
+            set.add(name);
+        }
+
+        for (int i = 0; i < M; i++) {
+            String name = br.readLine();
+            if (set.contains(name)) {
+                list.add(name);
+                count++;
+            }
+        }
+
+        Collections.sort(list);
+
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i)).append("\n");
+        }
+
+        System.out.println(count);
+        System.out.print(sb);
+    }
+}
