f633e922dd0a30caca6f03119d732771adc66f0d
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-03-04 16:23:42 +0900

    [Gold IV] Title: 스도쿠, Time: 372 ms, Memory: 18860 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -9,86 +9,73 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[][] map;
-    static boolean solved = false;
+    static List<int []> list = new ArrayList<int []>();
+    static boolean solve = false;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st;
-
-        map = new int[9][9];
-        for(int i = 0; i<9; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j<9; j++){
+        int[][] map = new int[9][9];
+        for(int i=0; i<9; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<9; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j]==0){
+                    list.add(new int[] {i, j});
+                }
             }
         }
 
-        StringBuilder sb = new StringBuilder();
+        dfs(0, map);
 
-        dfs(0, 0);
-
-        for(int i = 0; i<9; i++){
-            for(int j = 0; j<9; j++){
-                sb.append(map[i][j]).append(" ");
-            }
-            sb.append("\n");
-        }
-        System.out.print(sb);
     }
 
-    static void dfs(int x, int y){
-        if (solved) return;
-
-        if (x == 9) { 
-            solved = true;
+    public static void dfs(int depth, int[][] map){
+        if(depth == list.size() && !solve){
+            solve = true;
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<9; i++){
+                for(int j=0; j<9; j++){
+                    sb.append(map[i][j]).append(" ");
+                }
+                sb.append("\n");
+            }
+            System.out.print(sb);
             return;
         }
 
-        if (y == 9) { 
-            dfs(x + 1, 0);
-            return;
-        }
-
-        if (map[x][y] == 0) {
-            for (int v = 1; v <= 9; v++) {
-                if (canPlace(x, y, v)) {
-                    map[x][y] = v;
-                    dfs(x, y + 1);
-                    if (solved) return;
-                    map[x][y] = 0; 
-                }
+        int[] cur = list.get(depth);
+        int x = cur[0];
+        int y = cur[1];
+        for(int i=1; i<=9; i++){
+            if(canPlace(x, y, i, map)){
+                map[x][y] = i;
+                dfs(depth+1, map);
+                map[x][y] = 0; // 백트래킹
+                if(solve) return;
             }
-        } else {
-            dfs(x, y + 1); 
         }
     }
 
-    public static boolean canPlace(int x, int y, int num){
-        for(int i = 0; i<9; i++){
-            if(map[x][i] == num){
-                return false;
-            }
+    public static boolean canPlace(int x, int y, int num, int[][] map){
+        // 가로 확인
+        for(int i=0; i<9; i++){
+            if(map[x][i] == num) return false;
         }
-        for(int i = 0; i<9; i++){
-            if(map[i][y] == num){
-                return false;
-            }
-        }
-        int set_row = (x / 3) * 3;
-        int set_col = (y / 3) * 3;
 
-        for (int i = set_row; i < set_row + 3; i++) {
-            for (int j = set_col; j < set_col + 3; j++) {
-                if (map[i][j] == num) {
-                    return false;
-                }
+        // 세로 확인
+        for(int i=0; i<9; i++){
+            if(map[i][y] == num) return false;
+        }
+
+        // 네모 확인
+        int nx = x/3 * 3;
+        int ny = y/3 * 3;
+        for(int i=nx; i<nx+3; i++){
+            for(int j=ny; j<ny+3; j++){
+                if(map[i][j] == num) return false;
             }
         }
         return true;
     }
 }
-
-
