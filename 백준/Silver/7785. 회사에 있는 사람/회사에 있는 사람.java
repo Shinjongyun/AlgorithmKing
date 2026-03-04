430b178df6411cca4ced0b1b5dceb3feaa1b57f1
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-03-04 17:32:48 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 656 ms, Memory: 50496 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -0,0 +1,45 @@
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N;
+    static Set<String> set = new HashSet<>();
+    static List<String> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String type = st.nextToken();
+
+            if(type.equals("leave") && set.contains(name)){
+                set.remove(name);
+            }
+            else if(type.equals("enter")){
+                set.add(name);
+            }
+        }
+
+        // set -> list
+        for (String name : set) {
+            list.add(name);
+        }
+        
+        Collections.sort(list, Collections.reverseOrder());
+
+        StringBuilder sb = new StringBuilder();
+        for (String name : list) {
+            sb.append(name).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
