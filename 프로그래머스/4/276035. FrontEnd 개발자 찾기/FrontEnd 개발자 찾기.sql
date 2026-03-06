a8b9d768333ce731cfc39c276131a939c25adfbb
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-03-07 00:44:11 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,11 @@
+-- 코드를 작성해주세요
+SELECT DISTINCT
+    d.ID,
+    d.EMAIL,
+    d.FIRST_NAME,
+    d.LAST_NAME
+FROM DEVELOPERS d
+JOIN SKILLCODES s
+    ON s.CATEGORY = 'Front End'
+WHERE (d.SKILL_CODE & s.CODE) != 0
+ORDER BY d.ID;
