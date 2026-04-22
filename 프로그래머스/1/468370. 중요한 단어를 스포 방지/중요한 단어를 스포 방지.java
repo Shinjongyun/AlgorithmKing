5d4a855fd454522d951a43f7767c9b57e1ec8ae6
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-04-22 20:16:02 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 8.84 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.java
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.java
@@ -1,69 +1,107 @@
 import java.util.*;
 
 class Solution {
+    
+    static int answer = 0;
+    static Set<String> noSpo =  new HashSet<>();
+    static String[] spoilers;
+    static StringBuilder sb;
+    static StringBuilder spoSb;
+    static boolean[] isSpo;
+    
     public int solution(String message, int[][] spoiler_ranges) {
-        int answer = 0;
-
-        HashSet<String> nonSpo = new HashSet<>();   // 스포가 아닌 구간에 등장한 단어
-        HashSet<String> opened = new HashSet<>();   // 스포 단어
-
-        StringBuilder sb = new StringBuilder(message);
-        boolean[] covered = new boolean[message.length()];
-
-        for (int[] cur : spoiler_ranges) {
-            for (int i = cur[0]; i <= cur[1]; i++) {
-                covered[i] = true;
-            }
-        }
-
-        String[] words = message.split(" ");
-        int idx = 0;
-
-        // boolean을 통해 스포 아닌 애들부터 처리
-        for (String word : words) {
-            int start = idx;
-            int end = idx + word.length() - 1;
-
-            boolean isSpoWord = false;
-            for (int j = start; j <= end; j++) {
-                if (covered[j]) {
-                    isSpoWord = true;
-                    break;
-                }
-            }
-
-            if (!isSpoWord) {
-                nonSpo.add(word);
-            }
-
-            idx += word.length() + 1;
-        }
-
-        // 돌면서 스포 처리
-        for (int[] cur : spoiler_ranges) {
+        
+        sb = new StringBuilder(message);
+        spoSb = new StringBuilder();
+        isSpo = new boolean[message.length()];
+        
+        // 스포 구간 배치
+        for(int[] cur : spoiler_ranges){
             int start = cur[0];
             int end = cur[1];
-
-            int sIdx = start;
-            while (sIdx > 0 && sb.charAt(sIdx - 1) != ' ') {
-                sIdx--;
-            }
-
-            int eIdx = end;
-            while (eIdx < message.length() - 1 && sb.charAt(eIdx + 1) != ' ') {
-                eIdx++;
-            }
-
-            String[] spoiler = message.substring(sIdx, eIdx + 1).split(" ");
-
-            for (String word : spoiler) {
-                if (!nonSpo.contains(word) && !opened.contains(word)) {
-                    answer++;
-                }
-                opened.add(word);
+            for(int i = start; i<=end; i++){
+                isSpo[i]  =true;
             }
         }
-
+        
+        // 스포 아닌 단어들 set에 넣기
+        for(int i=0; i<message.length();){
+            int end=i;
+            while(true){
+                if(end == message.length() -1) break;
+                if(sb.charAt(end) == ' '){
+                    end--;
+                    break;
+                }
+                end++;
+            }
+            
+            // 먼저 스포가 포함되어 있는 지 체크
+            boolean flag = false;
+            for(int k = i; k<= end; k++){
+                if(isSpo[k]) flag = true;
+            }
+            if(flag) {
+                i = end + 2; 
+                continue;
+            }
+            
+            // 스포가 포함되어 있지 않다면 단어 추출
+            StringBuilder word = new StringBuilder();
+            for(int k =i; k<=end; k++){
+                word.append(sb.charAt(k));
+            }
+            
+            String w = word.toString();
+            if(!noSpo.contains(w)) noSpo.add(w);
+            i = end + 2;
+        }
+        
+        // 스포일러 단어들 추출
+        for(int[] cur : spoiler_ranges){
+            int start = cur[0];
+            int end = cur[1];
+            
+            int sIdx = start;
+            while(true){
+                if(start == 0){
+                    sIdx = 0;
+                    break;
+                }
+                if(sb.charAt(start - 1) == ' '){
+                    sIdx = start;
+                    break;
+                }
+                start--;
+            }
+            
+            int eIdx = end;
+            while(true){
+                if(end == message.length() - 1){
+                    eIdx = message.length() -1;
+                    break;
+                }
+                if(sb.charAt(end) == ' '){
+                    eIdx = end -1;
+                    break;
+                }
+                end++;
+            }
+            
+            for(int i=sIdx; i<=eIdx; i++){
+                spoSb.append(sb.charAt(i));
+            }
+            spoSb.append(" ");
+        }
+        spoilers = spoSb.toString().split(" ");
+        
+        // 돌면서 스포일러 처리하기
+        for(String spoiler : spoilers){
+            if(!noSpo.contains(spoiler)){
+                answer++;
+                noSpo.add(spoiler);
+            }
+        }
         return answer;
     }
 }
