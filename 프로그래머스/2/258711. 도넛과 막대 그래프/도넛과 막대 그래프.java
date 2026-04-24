812241dc06966e4735f1aed72d167e2a69f1af8d
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-04-24 17:21:36 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 235.47 ms, Memory: 202 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -7,12 +7,9 @@ class Solution {
     static List<Edge>[] graph;
     static boolean[] isYou;
     static int me;
-    static int[] donutArr;
-    static int[] degree;
     
     static class Edge {
         int to;
-        boolean visited;
         
         public Edge(int to) {
             this.to = to;
@@ -23,15 +20,11 @@ class Solution {
         answer = new int[4];
         n = 0;
         
-        // n 추출
         for (int i = 0; i < edges.length; i++) {
             int num = Math.max(edges[i][0], edges[i][1]);
             n = Math.max(n, num);
         }
         
-        degree = new int[n + 1];
-        
-        // 그래프 세팅
         graph = new List[n + 1];
         for (int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<>();
@@ -44,21 +37,12 @@ class Solution {
             int to = edges[i][1];
             
             graph[from].add(new Edge(to));
-            
-            // 들어오는 간선이 있는 노드 체크
             isYou[to] = true;
-            
-            // 나가는 간선 개수
-            degree[from]++;
         }
         
-        donutArr = new int[n + 1];
-        
         // 생성 노드 찾기
-        // 기존: !isYou[i]만 봄
-        // 수정: 생성 노드는 들어오는 간선이 없고, 나가는 간선이 2개 이상
         for (int i = 1; i <= n; i++) {
-            if (!isYou[i] && degree[i] >= 2) {
+            if (!isYou[i] && graph[i].size() >= 2) {
                 me = i;
                 break;
             }
@@ -66,60 +50,36 @@ class Solution {
         
         answer[0] = me;
         
-        // 생성 노드에서 연결된 그래프 하나씩 탐색
+        // 핵심 수정: start는 me가 아니라 e.to
         for (Edge e : graph[me]) {
-            e.visited = true;
-            dfs(e.to, e.to, false, false);
+            dfs(e.to, e.to, false);
         }
         
         return answer;
     }
     
-    public static void dfs(int from, int start, boolean isStart, boolean hasBranch) {
+    public static void dfs(int from, int start, boolean isStart) {
         
-        // DFS 도중 나가는 간선이 2개 이상인 노드를 만나면 8자 가능성 있음
-        if (degree[from] >= 2) {
-            hasBranch = true;
-        }
-        
-        // 다시 시작점으로 돌아온 경우
-        if (from == start && isStart) {
-            
-            // 중간에 분기점이 있었다면 8자
-            if (hasBranch) {
-                donutArr[start]++;
-                
-                // 시작점 자체가 8자 중심인 경우
-                // 두 바퀴를 다 돌아야 8자로 카운트
-                if (degree[start] >= 2) {
-                    if (donutArr[start] == degree[start]) {
-                        answer[3]++;
-                    }
-                } 
-                // 시작점은 중심이 아니지만, 중간에 8자 중심을 만난 경우
-                else {
-                    answer[3]++;
-                }
-            } 
-            // 분기점 없이 시작점으로 돌아오면 도넛
-            else {
-                answer[1]++;
-            }
-            
+        // 8자 그래프
+        if (graph[from].size() >= 2) {
+            answer[3]++;
             return;
         }
-        
-        // 막대인 경우
+
+        // 막대 그래프
         if (graph[from].size() == 0) {
             answer[2]++;
             return;
         }
-        
-        for (Edge e : graph[from]) {
-            if (!e.visited) {
-                e.visited = true;
-                dfs(e.to, start, true, hasBranch);
-            }
+
+        // 도넛 그래프
+        if (from == start && isStart) {
+            answer[1]++;
+            return;
         }
+
+        // 나가는 간선이 1개인 경우 계속 이동
+        Edge next = graph[from].get(0);
+        dfs(next.to, start, true);
     }
 }
