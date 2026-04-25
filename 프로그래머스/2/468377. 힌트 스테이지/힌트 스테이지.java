c696d9a6adf3ca82b843c123f2e40ee9bb3c912a
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-04-25 22:36:41 +0900

    [level 2] Title: 힌트 스테이지, Time: 28.59 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.java
+++ 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.java
@@ -1,44 +1,44 @@
 import java.util.*;
 
 class Solution {
-    
-    static int answer = Integer.MAX_VALUE;
+
+    static int answer;
     static int n;
-    static int[] cnt;
-    
+    static int[][] cost;
+    static int[][] hint;
+
     public int solution(int[][] cost, int[][] hint) {
-        
+
+        this.cost = cost;
+        this.hint = hint;
+
         n = cost.length;
-        int totalCount = 1 << (n - 1);
-        cnt = new int[n]; 
-        
-        // 2^(n-1)의 case 완전 탐색
-        for (int mask = 0; mask < totalCount; mask++) {
-            
-            int[] cnt = new int[n];   // 각 case의 힌트권의 개수
-            int sum = 0;    // 각 case의 비용의 총합
+        answer = Integer.MAX_VALUE;
 
-            // 
-            for (int j = 0; j < n; j++) {
-                // 현재 스테이지에서 가진 힌트권 최대 사용
-                int use = Math.min(cnt[j], n - 1);
-                sum += cost[j][use];
+        int total = 1 << (n - 1);
 
-                // j번째 스테이지에서 번들을 사는 경우
-                if ((mask & (1 << j)) != 0) {
-                    sum += hint[j][0]; // 현재 스테이지 번들 가격
+        for (int mask = 0; mask < total; mask++) {
+            int sum = 0;
+            int[] count = new int[n];
 
-                    // 구매한 번들의 힌트권 수 동기화 
-                    for (int k = 1; k < hint[j].length; k++) {
-                        int ticketStage = hint[j][k] - 1;
-                        cnt[ticketStage]++;
+            for (int stage = 0; stage < n; stage++) {
+
+                int use = Math.min(n - 1, count[stage]);
+                sum += cost[stage][use];
+
+                if ((mask & (1 << stage)) != 0) {
+                    sum += hint[stage][0];
+
+                    for (int i = 1; i < hint[stage].length; i++) {
+                        int targetStage = hint[stage][i] - 1;
+                        count[targetStage]++;
                     }
                 }
             }
 
             answer = Math.min(answer, sum);
         }
-        
+
         return answer;
     }
 }
