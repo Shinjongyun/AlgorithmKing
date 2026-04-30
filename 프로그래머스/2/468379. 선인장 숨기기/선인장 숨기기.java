4dbdbd0cdebeb7fb8091bb25fc0ce3b2f6bb2b2f
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-04-30 17:43:08 +0900

    [level 2] Title: 선인장 숨기기, Time: 823.80 ms, Memory: 535 MB -BaekjoonHub

--- 프로그래머스/2/468379. 선인장 숨기기/선인장 숨기기.java
+++ 프로그래머스/2/468379. 선인장 숨기기/선인장 숨기기.java
@@ -5,77 +5,77 @@ class Solution {
     static int m, n, h, w;
     static int[] answer;
     static int INF = Integer.MAX_VALUE;
-    static boolean imDone;
     static int[][] grid;
+    static int[][] drops;
     
     public int[] solution(int m, int n, int h, int w, int[][] drops) {
         
-        this.m= m;
-        this.n=n;
-        this.h=h;
-        this.w=w;
-        
-        answer = new int[2];
+        answer = new int [2];
         grid = new int [m][n];
+        this.n = n;
+        this.h = h;
+        this.m = m;
+        this.w = w;
+        this.drops = drops;
         
-        for(int i=0; i<m; i++){
-                for(int j=0; j<n; j++){
-                    grid[i][j] = INF;
-                }
+        for(int i = 0; i<m; i++){
+            for(int j =0; j<n; j++){
+                grid[i][j] = INF;
             }
+        }
         
-        // 일단 빗방울 gird에 적용
         for(int i =0; i<drops.length; i++){
             int x = drops[i][0];
             int y = drops[i][1];
-            
-            grid[x][y] = i+1;
+            grid[x][y] = i + 1;
         }
         
-        // 이분탐색
-        int left = 1;
-        int right = drops.length;
-        while(left <= right){
-            int mid = (left + right) /  2;
-            int[][] sum = new int[m+1][n+1];
-            
-            for(int i=0; i<m; i++){
-                for(int j=0; j<n; j++){
-                    // 현재 칸 val = grid[i][j] 
-                    int val = 0;
-                    if (grid[i][j] <= mid) val = 1;
-                    // sum [i+1][j+1]은 격자 [i][j]까지의 누적합이다.
-                    sum[i + 1][j + 1] = val + sum[i][j + 1] + sum[i + 1][j] - sum[i][j];
-                }
-            }
-            
-            boolean flag = findSafeZone(sum);
-            
-            if(flag){
-                left = mid +1;
-            } else{
-                right = mid-1;
-            }
-        }
+        binary();
         
         return answer;
     }
     
- 
-    // 이거 가능?
-    public static boolean findSafeZone(int[][] sum){
-        // 윈도우로 올기면서 area가 0(비가 안떨어지는 경우)에 좌표 리턴
-        // 왜 i는 h부터인가 -> (1, 1) 그림을 그려보면서 미세한 숫자를 확인한다.
-        for(int i=h; i<=m ; i++){
-            for(int j=w; j<=n ; j++){
-                // 누적합을 통해서 오직 윈도우만의 area를 구합니다.
-                int area = sum[i][j] - sum[i-h][j] - sum[i][j-w] + sum[i-h][j-w];
-                if(area == 0){
-                    answer = new int[]{i-h, j-w};
+    public static void binary(){
+        
+        int low = 1;
+        int high = drops.length;
+        
+        
+        while(low <= high){
+            
+            int mid = (low + high) / 2;
+            int[][] sum = new int [m+1][n+1];
+            for(int i = 0; i<m; i++){
+                for(int j=0; j<n; j++){
+                    int val = 0;
+                    if(grid[i][j] <= mid) val = 1;
+                    sum[i+1][j+1] = val + sum[i+1][j] + sum[i][j+1] - sum[i][j];
+                }
+            }
+            
+            if (cal(sum)) low = mid + 1;
+            else high = mid - 1;
+        }
+    }
+    
+    public static boolean cal(int[][] sum) {
+
+        for (int i = 0; i + h - 1 < m; i++) {
+            for (int j = 0; j + w - 1 < n; j++) {
+
+                int area = sum[i + h][j + w]
+                         - sum[i][j + w]
+                         - sum[i + h][j]
+                         + sum[i][j];
+
+                if (area == 0) {
+                    answer[0] = i;
+                    answer[1] = j;
                     return true;
                 }
             }
         }
+
         return false;
     }
 }
