8071cf0fd79d17d113e8f5eea4b2b8ce8d834742
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-05-05 17:37:36 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.36 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -4,43 +4,56 @@ import java.io.*;
 class Solution {
     
     static int[] answer;
+    static List<Integer> list = new ArrayList<>();
+    static int now;
+    static Map<String, Integer> map = new HashMap<>();
     
     public int[] solution(String today, String[] terms, String[] privacies) {
-        Map<String, Integer> termMap = new HashMap<>();
-        for (String t : terms) {
-            String[] parts = t.split(" ");
-            termMap.put(parts[0], Integer.parseInt(parts[1]));
+        
+        now = toVal(today);
+        
+        for(String t : terms){
+            
+            String[] term = t.split(" ");
+            
+            map.put(term[0], Integer.parseInt(term[1]));
         }
-
-        int todayVal = toDays(today);
-        List<Integer> res = new ArrayList<>();
-
-        for (int i = 0; i < privacies.length; i++) {
-            String[] parts = privacies[i].split(" ");
-            String date = parts[0];
-            String type = parts[1];
-
-            int start = toDays(date);
-            int months = termMap.get(type);
-
-            int expireLast = start + months * 28 - 1;  // 마지막 유효 날짜 계산
-            if (expireLast < todayVal) {     // 오늘이 유효 날짜보다 크다면?
-                res.add(i + 1); 
+        
+        int idx = 1;
+        for(String p : privacies){
+            
+            String[] pri = p.split(" ");
+            
+            int past = toVal(pri[0]);
+            String type = pri[1];
+            
+            int dur = map.get(type) * 28 - 1;
+            
+            if(past + dur < now){
+                list.add(idx);
             }
+            idx++;
         }
-        answer = new int[res.size()];
-        for (int i = 0; i < res.size(); i++) {
-            answer[i] = res.get(i);
+        
+        answer = new int [list.size()];
+        idx = 0;
+        for(int cur : list){
+            answer[idx] = cur;
+            idx++;
         }
+        
         return answer;
     }
-
-    private int toDays(String ymd) {
-        String[] p = ymd.split("\\.");
-        int y = Integer.parseInt(p[0]);
-        int m = Integer.parseInt(p[1]);
-        int d = Integer.parseInt(p[2]);
-
-        return y * 12 * 28 + m * 28 + d;
+        
+    public static int toVal(String t){
+        String[] time = t.split("\\.");
+        
+        int count = 0;
+        
+        int y = (Integer.parseInt(time[0]) - 2000) * 12 * 28; 
+        int h = (Integer.parseInt(time[1] )- 1)  * 28;
+        int d = Integer.parseInt(time[2]);
+        count = y + h + d;
+        return count;
     }
 }
