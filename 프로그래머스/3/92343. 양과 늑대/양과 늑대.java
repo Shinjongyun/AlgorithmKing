ddc06612090f9084b49053f53fe05e31673a233f
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-05-08 20:18:29 +0900

    [level 3] Title: 양과 늑대, Time: 2.22 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,71 +1,67 @@
 import java.util.*;
 
 class Solution {
-    
+
     static int answer = 0;
-    static int n, m;
-    static boolean[] isWolf;
-    static boolean[][] graph;
-    static boolean[] visited;
-    
+    static int n;
+    static List<Integer>[] tree;
+    static int[] info;
+
     public int solution(int[] info, int[][] edges) {
-        
+        this.info = info;
         n = info.length;
-        answer = 0;
-        
-        isWolf = new boolean[n];
-        visited = new boolean[n];
-        
-        for(int i = 0; i < n; i++){
-            if(info[i] == 1) isWolf[i] = true;
+
+        tree = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            tree[i] = new ArrayList<>();
         }
-        
-        graph = new boolean[n][n];
-        
-        for(int i = 0; i < edges.length; i++){
-            int from = edges[i][0];
-            int to = edges[i][1];
-            
-            // 부모 -> 자식 방향만 저장
-            graph[from][to] = true;
+
+        for (int[] e : edges) {
+            tree[e[0]].add(e[1]);
         }
-        
-        List<Integer> list = new ArrayList<>();
-        list.add(0);
-        
-        dfs(0, 0, list);
-        
+
+        List<Integer> can = new ArrayList<>();
+
+        // 0번 노드의 자식들을 후보에 넣고 시작
+        for (int child : tree[0]) {
+            can.add(child);
+        }
+
+        dfs(1, 0, can);
+
         return answer;
     }
-    
-    public static void dfs(int sheep, int wolf, List<Integer> can){
-        
-        for(int i = 0; i < can.size(); i++){
-            
-            int cur = can.get(i);
-            
+
+    public static void dfs(int sheep, int wolf, List<Integer> can) {
+        answer = Math.max(answer, sheep);
+
+        for (int i = 0; i < can.size(); i++) {
+            int next = can.get(i);
+
             int nextSheep = sheep;
             int nextWolf = wolf;
-            
-            if(!isWolf[cur]) nextSheep++;
-            else nextWolf++;
-            
-            if(nextWolf >= nextSheep) continue;
-            
-            answer = Math.max(answer, nextSheep);
-            
-            List<Integer> nextCan = new ArrayList<>(can);
-            
-            // 이번에 방문한 노드는 후보에서 제거
-            nextCan.remove(i);
-            
-            // 현재 노드의 자식들을 후보에 추가
-            for(int j = 0; j < n; j++){
-                if(graph[cur][j]){
-                    nextCan.add(j);
-                }
+
+            if (info[next] == 1) {
+                nextWolf++;
+            } else {
+                nextSheep++;
             }
-            
+
+            if (nextWolf >= nextSheep) {
+                continue;
+            }
+
+            // 현재 후보 리스트 복사
+            List<Integer> nextCan = new ArrayList<>(can);
+
+            // 방문할 노드는 후보에서 제거
+            nextCan.remove(i);
+
+            // 방문한 노드의 자식들을 후보에 추가
+            for (int child : tree[next]) {
+                nextCan.add(child);
+            }
+
             dfs(nextSheep, nextWolf, nextCan);
         }
     }
