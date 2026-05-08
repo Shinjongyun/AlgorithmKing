9bf4df94c6ca4dc07ae1a64d017e60c924be52c0
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-05-08 17:59:17 +0900

    [level 2] Title: 리프 노드 수 최대화, Time: 63.26 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/468372. 리프 노드 수 최대화/리프 노드 수 최대화.java
+++ 프로그래머스/2/468372. 리프 노드 수 최대화/리프 노드 수 최대화.java
@@ -6,32 +6,38 @@ class Solution {
     static int dist_limit, split_limit;
 
     public long solution(int dist_limit, int split_limit) {
-        this.dist_limit = dist_limit ;
+        this.dist_limit = dist_limit;
         this.split_limit = split_limit;
         dfs(1, 1, 1, 0);
         return answer;
     }
     
-    // 현재의 분배 노드의 후보
-    // 현재까지의 분배도
-    // 현재까지의 리프 노드의 수
-    // 현재까지 사용된 분배 노드의 수
+    // cur 현재 level의 분배노드 수
+    // split 현재까지의 분배도
+    // 리프노드 수
+    // 현재까지 사용한 분배 노드의 수
     public static void dfs(int cur, long split, int leaf, int used){
         
-        if (split > split_limit) return;
+        if(split > split_limit) return;
         
+        int remain = dist_limit - used;
+        
+        if(dist_limit < used) return;
+
         answer = Math.max(answer, leaf);
         
-        for (int child = 2; child <=3; child++){
+        for(int node=2; node<=3; node++){
             
-            int remain = dist_limit - used;
+            // 이번 턴에 분배 할 노드 수
+            int thisTime = Math.min(cur, remain);
             
-            int nextNodes = Math.min(cur, remain);
-
-            // gpt: 시간 초과
-            if (nextNodes == 0) continue; 
+            int nextUsed = used + thisTime;
             
-            dfs(nextNodes * child, split*child, (leaf - nextNodes) + nextNodes * child, used + cur);
+            long nextSplit = split * node;
+            
+            int nextLeaf = leaf - thisTime + thisTime * node;
+            
+            dfs(thisTime * node, nextSplit, nextLeaf, nextUsed);
         }
     }
 }
