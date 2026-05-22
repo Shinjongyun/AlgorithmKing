5f8cd41a06aeb43318a85df0a338b9843100a2f1
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-05-22 21:38:28 +0900

    [level 2] Title: 바이러스 파이프, Time: 9.00 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/2/468373. 바이러스 파이프/바이러스 파이프.java
+++ 프로그래머스/2/468373. 바이러스 파이프/바이러스 파이프.java
@@ -2,86 +2,89 @@ import java.util.*;
 
 class Solution {
     
-    static int answer = 0;
+    static int k; 
+    static int n;
     static List<Edge>[] graph;
-    static int[] typeList;
-    static int K, N;
-    static int start;
-    
+    static int answer = 0;
+    static int infection;
+
     static class Edge{
-        
         int to;
         int type;
-        public Edge (int to, int type){
-            this.to=to;
-            this.type=type;
+        public Edge(int t, int type){
+            this.to = t;
+            this.type = type;
         }
     }
     
     public int solution(int n, int infection, int[][] edges, int k) {
         
-        K=k;
-        N=n;
-        start = infection;
+        this.n = n;
+        this.k = k;
+        this.infection = infection; 
         
-        graph = new List[n + 1];
+        graph = new List[n+1];
         for(int i=1; i<=n; i++){
             graph[i] = new ArrayList<>();
         }
-        typeList = new int[k+1];
         
-        for(int i=0; i<edges.length; i++){
-            graph[edges[i][0]].add(new Edge(edges[i][1], edges[i][2]));
-            graph[edges[i][1]].add(new Edge(edges[i][0], edges[i][2]));
+        for(int [] e : edges){
+            int from = e[0];
+            int to = e[1];
+            int type = e[2];
+            graph[from].add(new Edge(to, type));
+            graph[to].add(new Edge(from, type));
         }
         
-        back(1);
-        
+        int[] choice = new int[k];
+        back(0, choice);
         return answer;
     }
     
-    public static void back(int depth){
+    public static void back(int depth, int[] choice){
         
-        if(depth == K+1){
-            int count;
-            count = bfs(start);
+        if(depth == k){
+            int count = cal(choice);
             answer = Math.max(answer, count);
             return;
         }
         
-        for(int i = 1; i<=3; i++){
-            typeList[depth] = i;
-            back(depth+1);
-            typeList[depth] = 0;
+        for(int i= 1; i<=3; i++){
+            choice[depth] = i;
+            back(depth + 1, choice);
+            choice[depth] = 0; 
         }
     }
     
-    public static int bfs(int start){
-
-        boolean[] visited = new boolean[N + 1];
-        visited[start] = true;
-
-        for(int step = 1; step <= K; step++){
-            Queue<Integer> q = new LinkedList<>();
-
-            for(int i = 1; i <= N; i++){
-                if(visited[i]) q.add(i);
+    public static int cal(int[] choice){
+        
+        Queue<Integer> q  = new LinkedList<>(); 
+        boolean[] visited = new boolean[n+1];
+        visited[infection] = true;
+        
+        for(int i=0; i<k; i++){
+            
+            for(int j=1; j<=n; j++){
+                if(visited[j]) q.add(j);
             }
-
+            
+            int thisType = choice[i];
+            
             while(!q.isEmpty()){
+            
                 int cur = q.poll();
 
                 for(Edge e : graph[cur]){
-                    if(!visited[e.to] && e.type == typeList[step]){
+                    if(!visited[e.to] && e.type == thisType){
                         visited[e.to] = true;
                         q.add(e.to);
                     }
                 }
             }
         }
-
+        
         int count = 0;
-        for(int i = 1; i <= N; i++){
+        for(int i=1; i<=n; i++){
             if(visited[i]) count++;
         }
         return count;
