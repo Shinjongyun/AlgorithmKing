e132af677dab62d911dfff302ac46200ec05dfd8
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-06-15 19:44:19 +0900

    [level 2] Title: 리프 노드 수 최대화, Time: 75.84 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/468372. 리프 노드 수 최대화/리프 노드 수 최대화.java
+++ 프로그래머스/2/468372. 리프 노드 수 최대화/리프 노드 수 최대화.java
@@ -1,43 +1,43 @@
 import java.util.*;
 
 class Solution {
-    
-    static long answer = 1;
-    static int dist_limit, split_limit;
 
+    static int dist_limit;
+    static int split_limit; 
+    static long answer = Integer.MIN_VALUE;
+    
+    
     public long solution(int dist_limit, int split_limit) {
+        
         this.dist_limit = dist_limit;
         this.split_limit = split_limit;
-        dfs(1, 1, 1, 0);
-        return answer;
-    }
-    
-    // cur 현재 level의 분배노드 수
-    // split 현재까지의 분배도
-    // 리프노드 수
-    // 현재까지 사용한 분배 노드의 수
-    public static void dfs(int cur, long split, int leaf, int used){
         
+        dfs(1, 0, 1, 1);
+        return answer; 
+    }     
+    
+    // 이번 뎁스의 분배 노드 후보의 수 : hubo
+    // 현재까지의 분배 노드의 수 : dist
+    // 현재까지의 분배도 : split
+    // 현재까지의 리프노드의 수 : leaf
+    public void dfs(long hubo, long dist, long split, long leaf){
+        
+        if(dist > dist_limit) return;
         if(split > split_limit) return;
         
-        int remain = dist_limit - used;
-        
-        if(dist_limit < used) return;
-
         answer = Math.max(answer, leaf);
         
-        for(int node=2; node<=3; node++){
+        for(int i = 2; i<=3; i++){
             
-            // 이번 턴에 분배 할 노드 수
-            int thisTime = Math.min(cur, remain);
+            long thisTime = Math.min(hubo, dist_limit - dist);
             
-            int nextUsed = used + thisTime;
+            long nextHubo = thisTime * i;
+            long nextDist = dist + thisTime; 
+            long nextSplit = split * i;
+            long nextLeaf = leaf - thisTime + thisTime * i;
             
-            long nextSplit = split * node;
-            
-            int nextLeaf = leaf - thisTime + thisTime * node;
-            
-            dfs(thisTime * node, nextSplit, nextLeaf, nextUsed);
+            // 까기
+            dfs(nextHubo, nextDist, nextSplit, nextLeaf);
         }
     }
 }
