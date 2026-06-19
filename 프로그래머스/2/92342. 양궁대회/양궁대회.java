7da05ea5417d65f77db51062caafda07b1641244
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-06-19 20:13:25 +0900

    [level 2] Title: 양궁대회, Time: 5.43 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,75 +1,78 @@
 class Solution {
-
-    static int maxDiff;
-    static int[] answer;
+    
+    static int n; 
     static int[] info;
+    static int maxDiff = 0;
+    static int[] answer;
 
     public int[] solution(int n, int[] info) {
+        
+        this.n = n;
         this.info = info;
-        maxDiff = 0;
-        answer = new int[]{-1};
-
-        int[] ryan = new int[11];
-
+        
+        answer = null;
+        
+        int[] ryan = new int[info.length];
         back(0, ryan, n);
-
+        
+        if (answer == null) {
+            return new int[]{-1};
+        }
         return answer;
     }
 
     public static void back(int depth, int[] ryan, int remain) {
 
-        if (depth == 11) {
+        if (depth == 10) {
+            ryan[10] = remain;
 
-            // 화살 n발을 정확히 다 쏜 경우만 검사
-            if (remain != 0) return;
+            int result = cal(ryan);
 
-            int diff = cal(ryan);
-
-            if (diff <= 0) return;
-
-            if (diff > maxDiff || (diff == maxDiff && isBetter(ryan, answer))) {
-                maxDiff = diff;
-                answer = ryan.clone();
+            if (result > 0) {
+                if (result > maxDiff) {
+                    maxDiff = result;
+                    answer = ryan.clone();
+                } else if (result == maxDiff && isBetter(ryan, answer)) {
+                    answer = ryan.clone();
+                }
             }
 
             return;
         }
-
-        for (int i = 0; i <= remain; i++) {
-            ryan[10 - depth] = i;
-            back(depth + 1, ryan, remain - i);
-            ryan[10 - depth] = 0;
-        }
+        
+        for(int i=0; i<=remain; i++){
+            
+            ryan[depth] = i;
+            back(depth+1, ryan, remain - i);
+            
+        }   
     }
-
-    public static int cal(int[] ryan) {
-
-        int apeach = 0;
+    
+    public static int cal(int[] ryan){
+        
         int ryanTotal = 0;
-
-        for (int i = 0; i < 11; i++) {
-
-            if (info[i] == 0 && ryan[i] == 0) continue;
-
-            if (info[i] >= ryan[i]) {
-                apeach += 10 - i;
-            } else {
+        int apeachTotal = 0;
+        
+        for(int i=0; i<=10; i++){
+            if (ryan[i] > info[i]) {
                 ryanTotal += 10 - i;
+            } else if (info[i] > 0) {
+                apeachTotal += 10 - i;
             }
         }
-
-        return ryanTotal - apeach;
+        
+        return ryanTotal - apeachTotal; 
     }
+    
+     public static boolean isBetter(int[] ryan, int[] answer) {
+        if (answer == null) return true;
 
-    public static boolean isBetter(int[] ryan, int[] answer) {
-
-        // 아직 정답이 없는 경우
-        if (answer.length == 1 && answer[0] == -1) return true;
-
-        // 낮은 점수부터 비교
         for (int i = 10; i >= 0; i--) {
-            if (ryan[i] > answer[i]) return true;
-            if (ryan[i] < answer[i]) return false;
+            if (ryan[i] > answer[i]) {
+                return true;
+            } else if (ryan[i] < answer[i]) {
+                return false;
+            }
         }
 
         return false;
