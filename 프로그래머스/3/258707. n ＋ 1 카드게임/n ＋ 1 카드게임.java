1d90258315c9948df2927b70806a915baeb823bc
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-06-25 01:27:55 +0900

    [level 3] Title: n + 1 카드게임, Time: 7.64 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
@@ -2,64 +2,85 @@ import java.util.*;
 
 class Solution {
     
-    static int answer = 0;
+    static int coin;
+    static int[] cards;
     static int n;
-    static Set<Integer> hand = new HashSet<>();
-    static Set<Integer> can = new HashSet<>();
-    static int left;
+    static Set<Integer> hand = new HashSet<>(); 
+    static Set<Integer> hubo = new HashSet<>(); 
     
     public int solution(int coin, int[] cards) {
         
+        this.coin = coin;   
+        this.cards = cards;
         n = cards.length;
-        left = coin;
         
-        // 손에 넣고
-        for(int i= 0; i<n/3; i++){
-            hand.add(cards[i]);
+        for(int i=0; i<n/3; i++){
+            hand.add(cards[i]);    
         }
         
-        // 게임 시작
         int round = 1;
-        for(int i = n/3; i<cards.length; i+=2){
+        for(int step = n/3; step+1<cards.length; step+=2){
             
-            int one = cards[i];
-            int two = cards[i+1];
+            int first = cards[step];
+            int second = cards[step+1];
+            hubo.add(first);
+            hubo.add(second);
             
-            can.add(one);
-            can.add(two);
-            
-            // 코인 0개
-            if(removePair(hand, hand)){
+            // 손안에서 해결이 가능한 경우 - 가장 좋은 경우 
+            if(check()) {
                 round++;
-            }
-            // 코인 1개
-            else if (coin >= 1 && removePair(hand, can)){
-                round++;
-                coin--;
-            }
-            // 코인 2개
-            else if (coin >=2 && removePair(can, can)){
-                round++;
-                coin = coin - 2;
+                continue;
             }
             
-            // 코인도 안돼, 페어도 없어
-            else {
-                break;
+            boolean flag = false;
+            
+            // 한장만 되는 경우
+            for(int cur : hubo){
+                
+                int target = n+1-cur;
+                if(coin > 0 && hand.contains(target)){
+                    coin--;
+                    round++;
+                    flag = true;
+                    hubo.remove(cur);
+                    hand.remove(target);
+                    break;
+                }
             }
+            
+             if (flag) continue;
+            
+            // 손에 잇는 놈끼리 되는 결루
+            for(int cur:hubo){
+                
+                int target = n+1-cur;
+                if(coin - 1 >0 && hubo.contains(target)){
+                    coin-=2;
+                    round++;
+                    flag = true;
+                    hubo.remove(cur);
+                    hubo.remove(target);
+                    break;
+                }
+            }
+            
+            // flag true면 다음 라운드
+            if(flag) continue;
+            
+            break;
         }
-        answer = round;
-        return answer;
-    }
+        
+        return round;
+    }   
     
-    public static boolean removePair(Set<Integer> a, Set<Integer> b){
-        for(int n1 : a){
+    public static boolean check(){
+        for(int cur : hand){
             
-            int target = n + 1 - n1;
-            
-            if(b.contains(target)){
-                a.remove(n1);
-                b.remove(target);
+            int target = n+1-cur;
+            if(hand.contains(target)) {
+                
+                hand.remove(cur);
+                hand.remove(target);
                 
                 return true;
             }
