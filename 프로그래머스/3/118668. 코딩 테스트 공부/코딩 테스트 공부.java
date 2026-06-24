dd2e298a710a21f4b0eb379c5df52e0e2d3d93f7
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-06-24 19:31:53 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 13.06 ms, Memory: 57.7 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -2,61 +2,64 @@ import java.util.*;
 
 class Solution {
 
-    static int[][] dp;
-    static int maxAlp = 0;
-    static int maxCop = 0;
-    static int INF = Integer.MAX_VALUE;
-    
     public int solution(int alp, int cop, int[][] problems) {
-        
-        // dp를 쓰는 이유 -> 완탐이 되지 않음 비트마스킹 불가, dfs도 3^100이 되므로 불가
-        
-        for(int[] p : problems){
-            maxAlp = Math.max(maxAlp, p[0]);
-            maxCop = Math.max(maxCop, p[1]);
+
+        int maxAlp = 0;
+        int maxCop = 0;
+
+        for (int i = 0; i < problems.length; i++) {
+            maxAlp = Math.max(maxAlp, problems[i][0]);
+            maxCop = Math.max(maxCop, problems[i][1]);
         }
-        dp = new int [maxAlp+1][maxCop+1];
-        for(int i=0; i<=maxAlp; i++){
-            for(int j=0; j<=maxCop; j++){
-                dp[i][j] = INF;
-            }
-        }
-        
+
         alp = Math.min(alp, maxAlp);
         cop = Math.min(cop, maxCop);
-        
+
+        int[][] dp = new int[maxAlp + 1][maxCop + 1];
+
+        for (int i = 0; i <= maxAlp; i++) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+
         dp[alp][cop] = 0;
-        for(int i=alp; i<=maxAlp; i++){
-            for(int j=cop; j<=maxCop; j++){
-                
-                if(i+1 <= maxAlp){
-                    dp[i+1][j] = Math.min(dp[i+1][j], dp[i][j] + 1);
+
+        for (int al = alp; al <= maxAlp; al++) {
+            for (int co = cop; co <= maxCop; co++) {
+
+                if (dp[al][co] == Integer.MAX_VALUE) {
+                    continue;
                 }
-                
-                if(j+1 <= maxCop){
-                    dp[i][j+1] = Math.min(dp[i][j+1], dp[i][j] + 1);
+
+                // 알고력 공부
+                if (al + 1 <= maxAlp) {
+                    dp[al + 1][co] = Math.min(dp[al + 1][co], dp[al][co] + 1);
                 }
-                
-                for(int[] p : problems){
-                    
-                    int reqAl = p[0];
-                    int reqCo = p[1];
-                    int rwAl = p[2];
-                    int rwCo = p[3];
-                    int cost = p[4];
-                    
-                    if(reqAl <= i && reqCo <= j){
-                        
-                        int nextAl = Math.min(i+rwAl, maxAlp);
-                        int nextCo = Math.min(j+rwCo, maxCop);
-                        
-                        dp[nextAl][nextCo] = Math.min(dp[nextAl][nextCo], dp[i][j] + cost);
+
+                // 코딩력 공부
+                if (co + 1 <= maxCop) {
+                    dp[al][co + 1] = Math.min(dp[al][co + 1], dp[al][co] + 1);
+                }
+
+                // 문제 풀기
+                for (int i = 0; i < problems.length; i++) {
+
+                    int curAlp = problems[i][0];
+                    int curCop = problems[i][1];
+                    int rwAlp = problems[i][2];
+                    int rwCop = problems[i][3];
+                    int cost = problems[i][4];
+
+                    if (al >= curAlp && co >= curCop) {
+                        int nextAlp = Math.min(maxAlp, al + rwAlp);
+                        int nextCop = Math.min(maxCop, co + rwCop);
+
+                        dp[nextAlp][nextCop] =
+                                Math.min(dp[nextAlp][nextCop], dp[al][co] + cost);
                     }
                 }
-                
             }
         }
+
         return dp[maxAlp][maxCop];
-        
     }
 }
