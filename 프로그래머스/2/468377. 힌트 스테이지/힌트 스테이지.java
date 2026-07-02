f05aeef77f9d38f27fd3cc47de563b3f169de22e
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-07-02 19:35:55 +0900

    [level 2] Title: 힌트 스테이지, Time: 16.07 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.java
+++ 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.java
@@ -2,37 +2,36 @@ import java.util.*;
 
 class Solution {
 
+    static int n; 
+    static int answer = Integer.MAX_VALUE;
+    
     public int solution(int[][] cost, int[][] hint) {
 
-        int n = cost.length;
-        int total = 1 << (n - 1);
-        int answer = Integer.MAX_VALUE;
-
-        for (int mask = 0; mask < total; mask++) {
-
-            int[] hintCount = new int[n];
+        n = cost.length;
+        
+        int mask = 1 << (n - 1);
+        for(int step = 0; step<mask; step++){
+            
             int totalCost = 0;
-
-            for (int i = 0; i < n; i++) {
-
-                // 이번에 최대로 쓸 수 있는 힌트권의 수
-                int use = Math.min(hintCount[i], n - 1);
-                totalCost += cost[i][use];
-
-                if ((mask & (1 << i)) != 0) {
-
+            int[] coupons = new int [n];
+            for(int i=0; i<n; i++){
+                
+                // 참고로 0은 첫번째 스테이지 의미
+                // 지금 있는 쿠폰을 사용하기
+                int useCount = Math.min(coupons[i], n - 1);
+                totalCost += cost[i][useCount];      
+                
+                // 지금 사야 된다면
+                if((step & 1 << i) != 0){
                     totalCost += hint[i][0];
-
-                    for (int j = 1; j < hint[i].length; j++) {
-                        int target = hint[i][j] - 1;
-                        hintCount[target]++;
+                    for(int k=1; k<hint[i].length; k++){
+                        coupons[hint[i][k] - 1]++;
                     }
                 }
             }
-
             answer = Math.min(answer, totalCost);
+            
         }
-
-        return answer;
+        return answer; 
     }
 }
