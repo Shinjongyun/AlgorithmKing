7dab8cd7dd65f43b279a184938bd46b8bb17d2c8
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-07-17 19:56:07 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 3.54 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.java
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.java
@@ -2,106 +2,84 @@ import java.util.*;
 
 class Solution {
     
-    static int answer = 0;
-    static Set<String> noSpo =  new HashSet<>();
-    static String[] spoilers;
-    static StringBuilder sb;
-    static StringBuilder spoSb;
-    static boolean[] isSpo;
+    static int answer;
+    static Set<String> set = new HashSet<>();
+    static boolean[] isSpoiler;
     
     public int solution(String message, int[][] spoiler_ranges) {
         
-        sb = new StringBuilder(message);
-        spoSb = new StringBuilder();
-        isSpo = new boolean[message.length()];
+        StringBuilder sb = new StringBuilder(message);
+        isSpoiler = new boolean[sb.length()];
         
-        // 스포 구간 배치
-        for(int[] cur : spoiler_ranges){
-            int start = cur[0];
-            int end = cur[1];
-            for(int i = start; i<=end; i++){
-                isSpo[i]  =true;
+        for(int[] s: spoiler_ranges){
+            int x = s[0];
+            int y = s[1]; 
+            
+            for(int i=x; i<=y; i++){
+                isSpoiler[i] = true;
             }
         }
         
-        // 스포 아닌 단어들 set에 넣기
-        for(int i=0; i<message.length();){
-            int end=i;
-            while(true){
-                if(end == message.length() -1) break;
-                if(sb.charAt(end) == ' '){
-                    end--;
-                    break;
+        // 한글자씩 돌아가기
+        for(int i=0; i<sb.length(); i++){
+            
+            // 빈칸이 아니고, 스포도 아닌 경우
+            if(sb.charAt(i) != ' '){
+                
+                boolean noSpo = true;
+                StringBuilder word = new StringBuilder();
+                while(i < sb.length() && sb.charAt(i) != ' '){
+                    
+                    if(isSpoiler[i]) noSpo = false;
+                    word.append(sb.charAt(i));
+                    i++;
                 }
-                end++;
+                
+                if(noSpo && !set.contains(word.toString())) set.add(word.toString());
             }
-            
-            // 먼저 스포가 포함되어 있는 지 체크
-            boolean flag = false;
-            for(int k = i; k<= end; k++){
-                if(isSpo[k]) flag = true;
-            }
-            if(flag) {
-                i = end + 2; 
-                continue;
-            }
-            
-            // 스포가 포함되어 있지 않다면 단어 추출
-            StringBuilder word = new StringBuilder();
-            for(int k =i; k<=end; k++){
-                word.append(sb.charAt(k));
-            }
-            
-            String w = word.toString();
-            if(!noSpo.contains(w)) noSpo.add(w);
-            i = end + 2;
         }
         
-        // 스포일러 단어들 추출
-        for(int[] cur : spoiler_ranges){
-            int start = cur[0];
-            int end = cur[1];
+        // 이제 스포 단어 찾으면서 세기
+        int count = 0;
+        for(int i=0; i<sb.length(); i++){
             
-            int sIdx = start;
-            while(true){
-                if(start == 0){
-                    sIdx = 0;
-                    break;
+            if(isSpoiler[i] && sb.charAt(i) != ' '){
+                int sI = i;
+                int eI = i;
+                for(int j=i; j>=0; j--){
+                    if(sb.charAt(j) == ' ') {
+                        sI = ++j;
+                        break; 
+                    }
+                    if(j==0) sI=j;
                 }
-                if(sb.charAt(start - 1) == ' '){
-                    sIdx = start;
-                    break;
+                
+                for(int j=i; j<sb.length(); j++){
+                    if(sb.charAt(j) == ' '){ 
+                        eI = --j;
+                        break; 
+                    }
+                    if(j==sb.length()-1) eI = j;
                 }
-                start--;
-            }
-            
-            int eIdx = end;
-            while(true){
-                if(end == message.length() - 1){
-                    eIdx = message.length() -1;
-                    break;
+                
+                StringBuilder spoilerWord = new StringBuilder();
+                for(int j=sI; j<=eI; j++){
+                    
+                    spoilerWord.append(sb.charAt(j));
+                    
                 }
-                if(sb.charAt(end) == ' '){
-                    eIdx = end -1;
-                    break;
+                
+                String[] spoilers = spoilerWord.toString().split(" ");
+                
+                for(int l=0; l<spoilers.length; l++){
+                    if(!set.contains(spoilers[l])){
+                        count++;
+                        set.add(spoilers[l]);
+                    } 
                 }
-                end++;
-            }
-            
-            for(int i=sIdx; i<=eIdx; i++){
-                spoSb.append(sb.charAt(i));
-            }
-            spoSb.append(" ");
-        }
-        spoilers = spoSb.toString().split(" ");
-        
-        // 돌면서 스포일러 처리하기
-        for(String spoiler : spoilers){
-            if(!noSpo.contains(spoiler)){
-                answer++;
-                noSpo.add(spoiler);
+                i = eI+1;
             }
         }
-        return answer;
+        return count;
     }
 }
