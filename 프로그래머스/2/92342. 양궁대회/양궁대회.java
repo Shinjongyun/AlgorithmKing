86200834fef4a3748c8b07ffebbd0ce027e14651
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-07-19 20:26:42 +0900

    [level 2] Title: 양궁대회, Time: 15.20 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,39 +1,51 @@
 class Solution {
-    
-    static int n; 
-    static int[] info;
-    static int maxDiff = 0;
-    static int[] answer;
 
+    static int[] answer = new int[11];
+    static int n;
+    static int[] info;
+    static int INF = Integer.MAX_VALUE;
+    static boolean found = false;
+    static int maxDiff = 0;
+    
     public int[] solution(int n, int[] info) {
         
         this.n = n;
         this.info = info;
         
-        answer = null;
-        
-        int[] ryan = new int[info.length];
-        back(0, ryan, n);
-        
-        if (answer == null) {
-            return new int[]{-1};
+        for(int i=0; i<11; i++){
+            answer[i] = INF;
         }
-        return answer;
+        
+        int remain = n;
+        int[] arr = new int [11];
+        back(0, remain, arr);
+        
+        boolean flag = false;
+        for(int i=0; i<11; i++){
+            if(answer[i] == INF) flag = true;
+        }
+        
+        if(flag) return new int[] {-1};
+        else return answer;
     }
+    
+    public static void back(int depth, int remain, int[] arr){
+        
+        if (depth == 11) {
 
-    public static void back(int depth, int[] ryan, int remain) {
+            // 화살을 전부 사용한 경우만 검사
+            if (remain == 0) {
 
-        if (depth == 10) {
-            ryan[10] = remain;
+                int diff = cal(arr);
 
-            int result = cal(ryan);
+                // 점수 차가 더 크거나
+                // 점수 차가 같고 낮은 점수를 더 많이 맞힌 경우
+                if (diff > maxDiff ||
+                    (diff == maxDiff && diff > 0 && check(arr))) {
 
-            if (result > 0) {
-                if (result > maxDiff) {
-                    maxDiff = result;
-                    answer = ryan.clone();
-                } else if (result == maxDiff && isBetter(ryan, answer)) {
-                    answer = ryan.clone();
+                    maxDiff = diff;
+                    answer = arr.clone();
+                    found = true;
                 }
             }
 
@@ -42,39 +54,42 @@ class Solution {
         
         for(int i=0; i<=remain; i++){
             
-            ryan[depth] = i;
-            back(depth+1, ryan, remain - i);
+            if(remain - i < 0) continue;
+            arr[depth] = i;
+            back(depth + 1, remain - i, arr);
+        }
+    }
+    
+    public static int cal (int[] arr){
+        int brown = 0;
+        int apeach  = 0;
+        
+        for(int i=0; i<11; i++){
             
-        }   
-    }
-    
-    public static int cal(int[] ryan){
-        
-        int ryanTotal = 0;
-        int apeachTotal = 0;
-        
-        for(int i=0; i<=10; i++){
-            if (ryan[i] > info[i]) {
-                ryanTotal += 10 - i;
-            } else if (info[i] > 0) {
-                apeachTotal += 10 - i;
+            // 둘 다 0
+            if(arr[i] == 0 && info[i] == 0) continue;
+            // 브라운이 이긴 경우
+            else if(arr[i] > info[i]){
+                brown += 10 - i;
             }
+            else apeach += 10 - i;
+            
         }
         
-        return ryanTotal - apeachTotal; 
+        return brown - apeach;
     }
     
-     public static boolean isBetter(int[] ryan, int[] answer) {
-        if (answer == null) return true;
-
-        for (int i = 10; i >= 0; i--) {
-            if (ryan[i] > answer[i]) {
-                return true;
-            } else if (ryan[i] < answer[i]) {
-                return false;
+    public static boolean check (int[] arr){
+        
+        if(!found) return true;
+        
+        for(int i=10; i>=0; i--){
+            
+            if(answer[i] != arr[i]){
+                if(answer[i] < arr[i]) return true;
+                else return false;
             }
         }
-
-        return false;
+        return false; 
     }
 }
