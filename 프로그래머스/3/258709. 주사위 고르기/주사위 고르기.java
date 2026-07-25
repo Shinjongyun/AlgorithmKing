42f3a6af436a20ddd390cad0e376dcc804e9329e
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-07-25 15:25:06 +0900

    [level 3] Title: 주사위 고르기, Time: 86.48 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
@@ -4,109 +4,110 @@ class Solution {
     
     static int n; 
     static int[][] dice;
-    static int answer = 0;
     static int[] optimal;
+    static int answer = -1;
 
     public int[] solution(int[][] dice) {
         
-        this.dice = dice; 
+        this.dice = dice;
         n = dice.length;
+        optimal = new int [n/2];
         
-        int[] choice = new int[n/2];
-        optimal = new int[n/2];
-        back(0, choice, 0);
-        
-        for(int i=0; i<optimal.length; i++){
-            optimal[i]++;
-        }
-        
-        return optimal;
+        int[] choose = new int[n/2];
+        back(0, 0, choose);
+        return optimal; 
     }
-        
-    // 조합은 뭐다? 백트래킹이다.
-    public static void back(int depth, int[] choice, int idx){
+    
+    // 순열
+    public static void back (int depth, int idx, int[] choose){
         
         if(depth == n/2){
-            int result = cal(choice); 
+            int result = cal(choose);
             
             if(result > answer){
-                optimal = choice.clone();
-                answer = result; 
+                answer = result;
+                for(int i=0; i<choose.length; i++){
+                    optimal[i] = choose[i] + 1;
+                }
             }
             
             return; 
         }
         
         for(int i=idx; i<n; i++){
-            choice[depth] = i;
-            back(depth+1, choice, i+1);
-            choice[depth] = 0;
+            
+            choose[depth] = i;
+            back(depth+1, i+1, choose);
         }
     }
     
-    public static int cal(int[] choice){
+    public static int cal (int[] choose){
         
-        int m = n/2;
-        // 일단 A 부터 
-        int[][] dp = new int[m + 1][5001];
-
-        dp[0][0] = 1;
-
-        for (int i = 0; i < m; i++) {
-            int curIdx = choice[i];
-
-            for (int sum = 0; sum <= 5000; sum++) {
-                if (dp[i][sum] == 0) continue;
-
-                for (int k = 0; k < 6; k++) {
-                    int curNum = dice[curIdx][k];
-                    dp[i + 1][sum + curNum] += dp[i][sum];
+        // 전처리
+        boolean[] use = new boolean [n];
+        for(int i=0; i<choose.length; i++){
+            use[choose[i]] = true;
+        }
+        
+        int[][] dp = new int [n/2+1][501];
+        
+        for(int d=0; d<choose.length; d++){
+            int cur = choose[d];
+            
+            if(d == 0){
+                
+                for(int i=0; i<6; i++){
+                    dp[1][dice[cur][i]]++;   
+                }   
+                continue;
+            }
+            
+            for(int j=1; j<=400; j++){
+                for(int i=0; i<6; i++){
+                    dp[d+1][j + dice[cur][i]] += dp[d][j];
                 }
             }
         }
         
-        // 그 다음 B
-        int[][] dpB = new int[m + 1][5001];
         
-        int[] choiceB = new int [n/2];
-        boolean[] visited = new boolean [n];
-        for(int i=0; i<n/2; i++){
-            visited[choice[i]] = true;
-        }
+        int[] choose1 = new int[n/2];
         int idx = 0;
-        for(int i=0; i<n; i++){
-            if(!visited[i]) {
-                choiceB[idx] = i;
+        for(int i=0; i<use.length; i++){
+            if(idx == n/2) break;
+            
+            if(!use[i]){
+                choose1[idx] = i;
                 idx++;
             }
         }
         
-        dpB[0][0] = 1;
-
-        for (int i = 0; i < m; i++) {
-            int curIdx = choiceB[i];
-
-            for (int sum = 0; sum <= 5000; sum++) {
-                if (dpB[i][sum] == 0) continue;
-
-                for (int k = 0; k < 6; k++) {
-                    int curNum = dice[curIdx][k];
-                    dpB[i + 1][sum + curNum] += dpB[i][sum];
+        int[][] dp1 = new int [n/2+1][501];
+        
+        for(int d=0; d<choose1.length; d++){
+            int cur = choose1[d];
+            
+            if(d == 0){
+                
+                for(int i=0; i<6; i++){
+                    dp1[1][dice[cur][i]]++;   
+                }   
+                continue;
+            }
+            
+            for(int j=1; j<=400; j++){
+                for(int i=0; i<6; i++){
+                    dp1[d+1][j + dice[cur][i]] += dp1[d][j];
                 }
             }
         }
         
-        int win = 0; 
-        for (int aSum = 0; aSum <= 5000; aSum++) {
-            if (dp[m][aSum] == 0) continue;
-
-            for (int bSum = 0; bSum < aSum; bSum++) {
-                if (dpB[m][bSum] == 0) continue;
-
-                win += (long) dp[m][aSum] * dpB[m][bSum];
+        int count = 0;
+        for(int i=1; i<=500; i++){
+            int cur = dp[n/2][i];
+            for(int j=1; j<i; j++){
+                count += cur * dp1[n/2][j];
             }
         }
-
-        return win;
+        return count;
     }
 }
