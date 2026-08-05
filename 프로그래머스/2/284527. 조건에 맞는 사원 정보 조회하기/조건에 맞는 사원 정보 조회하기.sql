6e5be682319497bc19fabf7db11fcdf4a8a15f27
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-08-05 09:48:29 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,29 +1,17 @@
 -- 코드를 작성해주세요
 SELECT
-    T.SCORE,
-    T.EMP_NO,
-    T.EMP_NAME,
-    T.POSITION,
-    T.EMAIL
-FROM (
-    SELECT
-        SUM(G.SCORE) AS SCORE,
-        E.EMP_NO,
-        E.EMP_NAME,
-        E.POSITION,
-        E.EMAIL
-    FROM HR_EMPLOYEES E
-    JOIN HR_GRADE G
-        ON E.EMP_NO = G.EMP_NO
-    GROUP BY
-        E.EMP_NO,
-        E.EMP_NAME,
-        E.POSITION,
-        E.EMAIL
-) T
-JOIN (
-    SELECT
-        MAX(S.TOTAL_SUM) AS MS
+    SUM(G.SCORE) AS SCORE,
+    E.EMP_NO,
+    E.EMP_NAME,
+    E.POSITION,
+    E.EMAIL
+FROM HR_EMPLOYEES E
+JOIN HR_GRADE G
+    ON E.EMP_NO = G.EMP_NO
+GROUP BY
+    E.EMP_NO
+HAVING SUM(G.SCORE) = (
+    SELECT MAX(S.TOTAL_SUM)
     FROM (
         SELECT
             EMP_NO,
@@ -31,5 +19,4 @@ JOIN (
         FROM HR_GRADE
         GROUP BY EMP_NO
     ) S
-) M
-    ON T.SCORE = M.MS;
+);
