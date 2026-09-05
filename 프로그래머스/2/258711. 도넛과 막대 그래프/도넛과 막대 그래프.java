33406cb0b724ef78c517740d994677cfb514a614
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-09-06 01:16:03 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 232.54 ms, Memory: 222 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -2,93 +2,85 @@ import java.util.*;
 
 class Solution {
     
+    static List<Edge>[] graph;
     static int[] answer = new int [4];
-    static int n;
-    static List<Edge>[] graph; 
-    static int[] count;
     
     class Edge{
         int to;
-        boolean visited; 
+        boolean visited;
         Edge(int to){
             this.to = to;
-            this.visited = false;
+            visited = false;
         }
     }
     
     public int[] solution(int[][] edges) {
-       
-        n = 0;
+        
+        int n = 0;
         for(int[] e : edges){
             n = Math.max(n, e[0]);
             n = Math.max(n, e[1]);
         }
         
         graph = new List[n+1];
-        for(int i=1; i<=n; i++){
+        for(int i=1 ; i<=n; i++){
             graph[i] = new ArrayList<>();
         }
-        count = new int[n+1];
         
-        for(int[] e : edges){
+        for (int[] e : edges){
             graph[e[0]].add(new Edge(e[1]));
         }
         
-        boolean[] visited = new boolean[n+1];
-        for(int[] e : edges){
-            visited[e[1]] = true;
+        int start = 0;
+        boolean[] check = new boolean[n+1];
+        for(int i=0; i<edges.length; i++){
+            int you = edges[i][1];
+            check[you] = true;
         }
+        
         for(int i=1; i<=n; i++){
-            if(!visited[i] && graph[i].size() >= 2){
-                answer[0] = i;
-                break;
+            if(!check[i] && graph[i].size() >= 2){
+                start = i;
+                answer[0] = start; 
+                break; 
             }
         }
         
-        for(Edge e : graph[answer[0]]){
+        for(Edge e : graph[start]){
             bfs(e.to);
         }
+        
         return answer;
     }
     
-    public static void bfs(int start){
-        
-        
+    public static void bfs (int start){
         
         Queue<Integer> q = new LinkedList<>();
         q.add(start);
-        count[start]++;
-        boolean isPal = false; 
         
+        boolean isPal = false;
         while(!q.isEmpty()){
             
             int cur = q.poll();
             
-            // System.out.print(cur + " ");
-            
-            if(graph[cur].size() == 2) isPal = true;
-            
-            if(graph[cur].size() == 0){
-                answer[2]++;
+            if(graph[cur].size() >= 2){
+                answer[3]++;
                 return;
             }
             
+            if(graph[cur].size() == 0){
+                answer[2]++;
+                return; 
+            }
+            
             for(Edge e : graph[cur]){
-                
-                if(e.to == start && isPal){
-                    answer[3]++;
-                    return;
-                }
-                
-                if(e.to == start && !isPal){
-                    answer[1]++;
-                    return; 
-                }
                 
                 if(e.visited) continue;
                 e.visited = true;
                 q.add(e.to);
             }
         }
+        
+        answer[1]++;
     }
 }
