d78994ede815d002847374d119b2c9d0fb54405a
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-09-06 01:45:38 +0900

    [level 3] Title: 양과 늑대, Time: 2.92 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,71 +1,72 @@
 import java.util.*;
 
 class Solution {
-
-    static List<Edge>[] graph; 
-    static int n;
-    static int[] info;
+    
+    static List<Edge>[] graph;
     static int answer = 0;
+    static boolean[] isWolf;
+    static int n;
     static boolean[] visited;
     
-    static class Edge{
+    class Edge{
         int to;
-        public Edge(int to){
+        Edge(int to){
             this.to = to;
         }
     }
     
     public int solution(int[] info, int[][] edges) {
         
-        this.info = info;
         n = info.length;
+        isWolf = new boolean [n];
+        visited = new boolean[n];
         
-        visited = new boolean [n];
         graph = new List[n];
         for(int i=0; i<n; i++){
-            graph[i] = new ArrayList<>(); 
+            graph[i] = new ArrayList<>();
+        }
+        
+        int idx = 0;
+        for(int i : info){
+            if(i == 1) isWolf[idx] = true;
+            idx++;
         }
         
         for(int[] e : edges){
-            int from = e[0];
-            int to = e[1];
-            
-            graph[from].add(new Edge(to));
-            graph[to].add(new Edge(from));
+            graph[e[0]].add(new Edge(e[1]));
         }
         
-        List<Integer> list = new ArrayList<>();
-        visited[0] = true;
-        dfs(0, 0, 0, list);
+        List<Integer> candidate = new ArrayList<>();
+        dfs(0, candidate, 1, 0);
+        
         return answer;
     }
     
-    public static void dfs (int cur, int sCount, int wCount, List<Integer> past){
+    public static void dfs(int cur, List<Integer> candidate, int sheep, int wolf){
         
-        List<Integer> list = new ArrayList<>(past);
+        answer = Math.max(answer, sheep);
         
-        if(info[cur] == 0) sCount++;
-        else wCount++; 
+        List<Integer> candi = new ArrayList<>(candidate);
         
-        answer = Math.max(answer, sCount); 
-        
-        // 안 왔으면 후보에 넣기
         for(Edge e : graph[cur]){
-            if(!visited[e.to]){
-                list.add(e.to);
-            }
+            if(visited[e.to]) continue;
+            candi.add(e.to);
         }
         
-        for(int i=0; i<list.size(); i++){
+        for(int i=0; i<candi.size(); i++){
+            int next = candi.get(i);
+            if(visited[next]) continue;
             
-            if(visited[list.get(i)]) continue;
-            
-            // 다음 갈 곳이 늑대인 경우
-            if(info[list.get(i)] == 1 && sCount - wCount == 1) continue;
-            
-            visited[list.get(i)] = true;
-            dfs(list.get(i), sCount, wCount, list);
-            visited[list.get(i)] = false;
+            visited[next]= true;
+            if(isWolf[next]){
+                if(sheep > wolf + 1){
+                    dfs(next, candi, sheep, wolf + 1);
+                }
+            }
+            else{
+                dfs(next, candi, sheep + 1, wolf);
+            }
+            visited[next] = false; 
         }
     }
 }
