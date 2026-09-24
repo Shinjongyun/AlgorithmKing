9061821224a63543dbb3dd3fe3a4178c0943db22
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-09-25 00:43:23 +0900

    [level 3] Title: 여행경로, Time: 18.22 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,37 +1,75 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
-    boolean[] visited;
-    ArrayList<String> allRoute; 
+    
+    static int n;
+    static String[][] tickets;
+    static String[] answer;
     
     public String[] solution(String[][] tickets) {
-        String[] answer = {};
-        int cnt = 0;
-        visited = new boolean[tickets.length];
-        allRoute = new ArrayList<>();
         
-        dfs("ICN", "ICN", tickets, cnt);
+        this.tickets = tickets;
+        n = tickets.length;
         
-        Collections.sort(allRoute);
-        answer = allRoute.get(0).split(" ");
+        boolean[] visited = new boolean[n];
+        
+        // 티켓 n장 -> 지나가는 공항 n+1개
+        String[] route = new String[n + 1];
+        
+        // 무조건 ICN에서 시작
+        route[0] = "ICN";
+        
+        dfs(1, visited, route);
         
         return answer;
     }
     
-    public void dfs(String start, String route, String[][] tickets, int cnt){
-        if(cnt == tickets.length){
-            allRoute.add(route);
+    public static void dfs(int depth, boolean[] visited, String[] route) {
+        
+        // 모든 티켓을 사용함
+        if(depth == n + 1) {
+            
+            if(answer == null) {
+                answer = route.clone();
+                return;
+            }
+            
+            // 현재 route가 answer보다 사전순으로 앞서는지 비교
+            for(int i = 0; i < route.length; i++) {
+                
+                int compare = route[i].compareTo(answer[i]);
+                
+                if(compare < 0) {
+                    answer = route.clone();
+                    return;
+                }
+                
+                if(compare > 0) {
+                    return;
+                }
+            }
+            
             return;
         }
         
-        for(int i=0; i<tickets.length; i++){
-            if(start.equals(tickets[i][0]) && !visited[i]){
+        for(int i = 0; i < n; i++) {
+            
+            if(visited[i]) continue;
+            
+            String from = tickets[i][0];
+            String to = tickets[i][1];
+            
+            // 현재 공항과 출발 공항이 같아야 함
+            if(route[depth - 1].equals(from)) {
+                
                 visited[i] = true;
-                dfs(tickets[i][1], route+" "+tickets[i][1], tickets, cnt+1);
+                route[depth] = to;
+                
+                dfs(depth + 1, visited, route);
+                
                 visited[i] = false;
+                route[depth] = null;
             }
         }
     }
-    
 }
