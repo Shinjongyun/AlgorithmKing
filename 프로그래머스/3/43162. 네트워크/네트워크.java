8c455076af09a3109f1d12cd6661e50923e28abf
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-09-24 23:00:31 +0900

    [level 3] Title: 네트워크, Time: 1.31 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,40 +1,66 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     
-    static boolean[] visited;
-    static int[][] graph;
-    static int N;
-    static boolean solved = false;
-    static int count = -1;
+    static int[] parent; 
+    static List<Edge>[] graph; 
+    
+    class Edge{
+        int to;
+        Edge(int to){
+            this.to = to; 
+        }
+    }
     
     public int solution(int n, int[][] computers) {
+        int answer = 0;
+        parent = new int[n+1];
         
-        N = n;
-        visited = new boolean[n];
-        graph = computers;
-
-        while(!solved){
-            int index = done();
-            dfs(index);
-            count++;
+        for(int i=1; i<=n; i++){
+            parent[i] = i; 
         }
         
-        int answer = count;
+        graph = new List[n+1]; for(int i=1; i<=n; i++) graph[i] = new ArrayList<>(); 
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i != j && computers[i][j] == 1){
+                    graph[i+1].add(new Edge(j+1)); 
+                }
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i != j && computers[i][j] == 1){
+                    union(i+1, j+1); 
+                }
+            }
+        }
+        
+        Set<Integer> set = new HashSet<>();
+        for(int i=1; i<=n; i++){
+            int f = find(i); 
+            if(!set.contains(f)){
+                answer++;
+                set.add(find(f));
+            }  
+        }
+        
         return answer;
     }
-    public static void dfs(int index){
-        visited[index] = true;
-        for(int i =0; i<graph[index].length; i++){
-            if(graph[index][i]==1 && !visited[i]) dfs(i);  
-        }
+    
+    public static int find(int x){
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]); 
     }
-    public static int done(){
-        for(int i=0; i <N; i++){
-            if(!visited[i]) return i;
-        }
-        solved = true;
-        return 0;
+    
+    public static void union(int a, int b){
+        int rootA = find(a);
+        int rootB = find(b); 
+        
+        if(rootA == rootB) return; 
+        
+        parent[rootA] = rootB;
     }
 }
