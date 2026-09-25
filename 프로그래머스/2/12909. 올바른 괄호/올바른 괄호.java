26af818b2003059bcd6405f6f0ddeda69fd91d3d
신종윤 <162339483+Shinjongyun@users.noreply.github.com>
2026-09-25 11:34:56 +0900

    [level 2] Title: 올바른 괄호, Time: 11.14 ms, Memory: 58 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '(') stack.push('(');
+            else{
+                if(stack.isEmpty() || stack.peek() == ')'){
+                    answer = false; 
+                    break;
+                    
+                }
+               
+                else{
+                    stack.pop();
+                    
+                }
+            }
+        }
+        
+        if(!stack.isEmpty()) answer = false; 
+
+        return answer;
+    }
+}
